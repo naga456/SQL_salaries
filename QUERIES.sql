b34aa417ebd2c715e3f717a4fe42ab078bb2a749
naga456 <wl0305@hotmail.com>
2019-07-23 11:56:25 -0400

    fixed query

--- QUERIES.sql
+++ QUERIES.sql
@@ -1,15 +1,15 @@
-SELECT COUNT(*) FROM EMPLOYEES
-SELECT * FROM SALARIES
+SELECT COUNT(*) FROM EMPLOYEES;
+SELECT * FROM SALARIES;
 --1. List the following details of each employee: employee number, last name, first name, gender, and salary.
 SELECT E.EMP_NO, E.LAST_NAME, E.FIRST_NAME, E.GENDER, S.SALARY
 FROM EMPLOYEES E 
 INNER JOIN SALARIES S ON E.EMP_NO = S.EMP_NO
-ORDER BY E.EMP_NO
+ORDER BY E.EMP_NO;
 
 --2. List employees who were hired in 1986.
 SELECT E.EMP_NO, E.LAST_NAME, E.FIRST_NAME, E.HIRE_DATE
 FROM EMPLOYEES E
-WHERE DATE_PART('YEAR',E.HIRE_DATE) =1986
+WHERE DATE_PART('YEAR',E.HIRE_DATE) =1986;
 
 --3. List the manager of each department with the following information: department number, 
 --department name, the manager's employee number, last name, first name, and start and end employment dates.
@@ -18,7 +18,7 @@ SELECT D.DEPT_NO, D.DEPT_NAME, DM.EMP_NO, E.LAST_NAME, E.FIRST_NAME, DM.FROM_DAT
 FROM DEPARTMENTS D 
 LEFT JOIN DEPT_MANAGER DM ON D.DEPT_NO = DM.DEPT_NO
 INNER JOIN EMPLOYEES E ON DM.EMP_NO = E.EMP_NO
-ORDER BY D.DEPT_NAME
+ORDER BY D.DEPT_NAME;
 
 --4. List the department of each employee with the following information: 
 -- employee number, last name, first name, and department name.
@@ -30,3 +30,7 @@ LEFT JOIN EMPLOYEES E ON DE.EMP_NO = E.EMP_NO
 ORDER BY E.EMP_NO
 
 --5. List all employees whose first name is "Hercules" and last names begin with "B."
+SELECT E.EMP_NO, E.FIRST_NAME, E.LAST_NAME
+FROM EMPLOYEES E
+WHERE E.FIRST_NAME='Hercules' and E.LAST_NAME LIKE 'B%'
+ORDER BY E.EMP_NO;
